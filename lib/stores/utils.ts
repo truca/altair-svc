45fafb6f71547e9c111f54fb1ad8110c2ba50d74
Sebastian Stuardo <sebafpenas@falabella.cl>
2025-02-18 17:46:52 -0300

    [NO-JIRA][Sebastian Stuardo] comment function getDbOptions

--- lib/stores/utils.ts
+++ lib/stores/utils.ts
@@ -1,18 +1,18 @@
 import { FirestoreStore, FirestoreOptions } from "./FirestoreStore";
 import { Store } from "./types";
 
-const serviceAccount = require("../../serviceAccountKey.json");
+// const serviceAccount = require("../../serviceAccountKey.json");
 
 export type DbTypes = "firestore";
 
-export function getDbOptions(dbType: DbTypes) {
-  switch (dbType) {
-    case "firestore":
-      return serviceAccount;
-    default:
-      throw new Error("Unsupported database type");
-  }
-}
+// export function getDbOptions(dbType: DbTypes) {
+//   switch (dbType) {
+//     case "firestore":
+//       return serviceAccount;
+//     default:
+//       throw new Error("Unsupported database type");
+//   }
+// }
 
 export function createStore(dbType: DbTypes, options: FirestoreOptions): Store {
   switch (dbType) {

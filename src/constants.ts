69a020f11d2f84b71e7530821cc380e0c97a2cb5
Sebastian Stuardo <sebafpenas@falabella.cl>
2025-05-02 13:32:37 -0400

    [VASS-1268][Sebastian Stuardo] CEP: Create new nomenclatarute for the planner

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+/**
+ * Application constants
+ *
+ * This file contains all the constants used across the application
+ */
+
+export const constants = {
+  COLLECTIONS_DATABASES: {
+    SERVICE: "Service",
+    BRAND: "Brand",
+    CAMPAIGN_GROUP: "CampaignGroup",
+    CATEGORY: "Category",
+    PRODUCT_MANAGER: "ProductManager",
+    PROFILE: "Profile",
+    SELLER: "Seller",
+    SUBCATEGORY: "SubCategory",
+  },
+};
